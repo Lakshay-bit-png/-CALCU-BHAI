1d3ac444ed3a88841a024b75a0690e9c83ee5276
Lakshay <135457408+Lakshay-bit-png@users.noreply.github.com>
2023-07-08 16:36:29 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,29 +1,38 @@
-var startTime;
+var animationDuration = 2;
+var loaderElement = document.getElementById('video-vanish');
+var animationInterval;
 
 window.addEventListener('load', function() {
-  
-
-  if (startTime) {
-    var loadingTime = performance.now() - startTime;
-    var animationDuration = loadingTime / 1000; 
-    
-  
-    document.getElementById('video-vanish').style.animationDuration = animationDuration + 's';
-  }
+  // Website loaded
+  var completionMessage = document.createElement('div');
+  completionMessage.textContent = 'Website fully loaded!';
+  completionMessage.style.color = '#fff';
+  completionMessage.style.fontSize = '24px';
+  document.body.appendChild(completionMessage);
 });
 
 window.addEventListener('progress', function(event) {
   if (event.lengthComputable) {
     var percentComplete = (event.loaded / event.total) * 100;
 
-    if (percentComplete >= 80 && !startTime) {
-        document.getElementById('video-vanish').style.top='-1000px';
-      startTime = performance.now();
-    }
-
-    if (percentComplete >= 100) {
-      
-      startTime = null;
+    if (percentComplete < 100) {
+    
+      loaderElement.style.animation = `slide-up ${animationDuration}s linear infinite`;
+    } else {
+      document.getElementById("video-vanish").style.top="-1000px";
+      loaderElement.style.animation = 'none';
     }
   }
 });
+
+function updateAnimationDuration(newDuration) {
+  
+  animationDuration = newDuration;
+  loaderElement.style.animationDuration = `${animationDuration}s`;
+}
+
+
+animationInterval = setInterval(function() {
+  var randomDuration = Math.floor(Math.random() * 4) + 1; 
+  updateAnimationDuration(randomDuration);
+}, 2000);
