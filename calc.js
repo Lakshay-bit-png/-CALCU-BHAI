6ca91341dcdddde00af75bec7d0f211c6ae568bf
Lakshay <135457408+Lakshay-bit-png@users.noreply.github.com>
2023-07-08 15:44:51 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 window.onload = function() {setTimeout(function(){document.getElementById("video-vanish").classList.add('slide-animation');
 document.getElementById("video-vanish").style.top='-1000px';
-},100);
+},2000);
 
 
 
