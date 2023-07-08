b93d44fd3ea99f8ecd91372be527931c53f5332c
Lakshay <135457408+Lakshay-bit-png@users.noreply.github.com>
2023-07-08 16:50:10 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,30 +1,18 @@
-var animationDuration = 2;
-var loaderElement = document.getElementById('video-vanish');
-var animationInterval;
 
-
-window.addEventListener('progress', function(event) {
-  if (event.lengthComputable) {
-    var percentComplete = (event.loaded / event.total) * 100;
-
-    if (percentComplete < 100) {
-    
-      loaderElement.style.animation = `slide-up ${animationDuration}s linear infinite`;
-    } else {
-      document.getElementById("video-vanish").style.top="-1000px";
-      loaderElement.style.animation = 'none';
-    }
-  }
-});
-
-function updateAnimationDuration(newDuration) {
   
-  animationDuration = newDuration;
-  loaderElement.style.animationDuration = `${animationDuration}s`;
-}
+  window.addEventListener('progress', function(event) {
+    if (event.lengthComputable) {
+      var percentComplete = (event.loaded / event.total) * 100;
+      var loaderElement = document.getElementById('video-vanish');
+  
+      if (percentComplete >= 80 && percentComplete <= 100) {
 
-
-animationInterval = setInterval(function() {
-  var randomDuration = Math.floor(Math.random() * 4) + 1; 
-  updateAnimationDuration(randomDuration);
-}, 2000);
+        loaderElement.classList.add('.slide');
+      } else {
+      
+        loaderElement.classList.remove('animation');
+        loaderElement.style.top="-1000px";
+      }
+    }
+  });
+  
