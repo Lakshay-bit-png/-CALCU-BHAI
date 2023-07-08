662b8fd792b401ee5a12a63870921d867d161fc5
Lakshay <135457408+Lakshay-bit-png@users.noreply.github.com>
2023-07-08 17:12:04 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,15 +1,20 @@
 
-window.addEventListener('progress', function(event) {
-  if (event.lengthComputable) {
-    var percentComplete = (event.loaded / event.total) * 100;
-
-    if (percentComplete >= 80 && percentComplete < 100 && !startTimestamp) {
-      // Start measuring time when 80% loading progress is reached
-      document.getElementById('video-vanish').style.animation = 'slide-up';
-      document.getElementById('video-vanish').style.top="-1000px";
-      
+  
+  window.addEventListener('progress', function(event) {
+    if (event.lengthComputable) {
+      var percentComplete = (event.loaded / event.total) * 100;
+      var loaderElement = document.getElementById('video-vanish');
+  
+      if (percentComplete >= 80 && percentComplete <= 100) {
+        // Calculate the animation duration within the desired range
+        var totalDuration = 2; // Total duration in seconds
+        var animationDuration = ((percentComplete - 80) / 20) * totalDuration;
+        loaderElement.style.animation= 'slide-up';
+        loaderElement.style.animationDuration = animationDuration + 's';
+      } else {
+        // Stop the animation if not in the desired range
+        loaderElement.style.animation = 'none';
+      }
     }
-
-    
-  }
-});
+  });
+  
