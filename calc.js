7550bd50eb9f411faeff4162e9b8836cd7da18c8
Lakshay <135457408+Lakshay-bit-png@users.noreply.github.com>
2023-07-08 17:05:35 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,18 +1,15 @@
 
-  
-  window.addEventListener('progress', function(event) {
-    if (event.lengthComputable) {
-      var percentComplete = (event.loaded / event.total) * 100;
-      var loaderElement = document.getElementById('video-vanish');
-  
-      if (percentComplete >= 80 && percentComplete <= 100) {
+window.addEventListener('progress', function(event) {
+  if (event.lengthComputable) {
+    var percentComplete = (event.loaded / event.total) * 100;
 
-        loaderElement.classList.add('.slide');
-      } else if(percentComplete == 100 ) {
+    if (percentComplete >= 80 && percentComplete < 100 && !startTimestamp) {
+      // Start measuring time when 80% loading progress is reached
+      document.getElementById('video-vanish').style.animation = 'slide-up';
+      document.getElementById('video-vanish').style.top="-1000px";
       
-        loaderElement.classList.remove('.slide');
-        loaderElement.style.top="-1000px";
-        console.log("1000");      }
     }
-  });
-  
+
+    
+  }
+});
