0810cc0c198ddef97e2f1cd1f6fb3a99d71328d8
Lakshay <135457408+Lakshay-bit-png@users.noreply.github.com>
2023-07-08 17:36:22 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,4 +1,8 @@
-
+window.addEventListener('load', function() {
+    var loaderElement = document.getElementById('video-vanish');
+    loaderElement.style.top="-1000px";
+  });
+  
   
   window.addEventListener('progress', function(event) {
     if (event.lengthComputable) {
@@ -7,8 +11,8 @@
       if (percentComplete >= 80) {
         // Apply animation to loader element
         var loaderElement = document.getElementById('video-vanish');
-        loaderElement.classList.add('.slide');
-        loaderElement.style.top='-1000px';
+        loaderElement.classList.add('animation');
+        
       }
     }
   });
